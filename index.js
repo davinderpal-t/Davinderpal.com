c5f2fa4eab9683dd333b8f02f06fa5657e11d1e8
davinderpal-t <davindertoor77@gmail.com>
2022-08-12 21:31:27 -0500

    Fixed Routing v2

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const product = require("./api/product");
+
+app.use(express.static("public"));
+//app.use(express.json({ extended: false }));
+
+app.use("/api/product", product);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log("Listening at: http://localhost:" + PORT);
+});

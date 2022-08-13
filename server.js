4ea034e86d017238b1e1ad6ac444823478c8f45e
davinderpal-t <davindertoor77@gmail.com>
2022-08-12 21:06:11 -0500

    Added PORT

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 5050;
 
 app.use(express.static("public"));
-app.use(express.json({limit: "1mb"}));
+app.use(express.json({ limit: "1mb" }));
 
 app.get("/stuff", (req, res) => {
     res.json({
@@ -10,6 +11,10 @@ app.get("/stuff", (req, res) => {
         "Age": "18",
         "Favorite Series": "Pokemon"
     });
+});
+
+app.listen(PORT, () => {
+    console.log("Listening at: " + PORT);
 });
 
 module.exports = app;

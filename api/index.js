cf0de5f08c12add69e902bb592ed0dd0d1d65242
davinderpal-t <davindertoor77@gmail.com>
2022-08-12 21:48:51 -0500

    Testing

--- api/index.js
+++ api/index.js
@@ -6,9 +6,11 @@ const app = express();
 
 //app.use("/api/product", product);
 app.get('/api', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Type', 'text/json');
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: <a href=""></a>`);
+    res.json({
+        "status": "sus"
+    });
 });
 
 module.exports = app;

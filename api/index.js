24fc577458fd26e740f028a4f8b212aceddc59b0
davinderpal-t <davindertoor77@gmail.com>
2022-08-12 21:56:07 -0500

    Fixed public paths

--- api/index.js
+++ api/index.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const app = express();
-//const product = require("./product");
 
-//app.use(express.json({ extended: false }));
-
-//app.use("/api/product", product);
+app.use(express.static("../public"));
 app.get('/api', (req, res) => {
     res.setHeader('Content-Type', 'text/json');
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');

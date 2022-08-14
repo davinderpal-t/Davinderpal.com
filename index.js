44dfc367be3cbdb58511f01692e0c8ff9c500a55
davinderpal-t <davindertoor77@gmail.com>
2022-08-13 19:54:57 -0500

    Switching from Vercel to Heroku

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static("../public"));
+let x = 0;
+
+app.use(express.static("public"));
 app.get('/api', (req, res) => {
+    x++;
     res.setHeader('Content-Type', 'text/json');
     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     res.json({
-        "status": "sus"
+        "status": x
     });
 });
-
-module.exports = app;
